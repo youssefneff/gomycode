b7cfb42d8cad25fcd8e022112b60691bffdd61ae
youssefneff <86619828+youssefneff@users.noreply.github.com>
2021-06-29 16:02:33 +0100

    Add files via upload

---
+++ assigning.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+x=5 #assign 5 to x
+print(x) #it returns 5
+
+
+# In[3]:
+
+
+x=10 #assign 10 to x
+x="welcome to pyhton world" #assigning a new variable to website
+print(x) #it returs welcome to python world
+
+
+# In[ ]:
+
+
+
+
