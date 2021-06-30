07454d6b90f06d36967439d98ade39dec7877e66
youssefneff <86619828+youssefneff@users.noreply.github.com>
2021-06-30 20:17:45 +0100

    Add files via upload

---
+++ question 2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a = int(input("input an integer:"))
+n1 = int( "%s" % a)
+n2 = int( "%s%s" % (a,a))
+n3 = int( "%s%s%s" % (a,a,a))
+print(n1+n2+n3)
+
+
+# In[ ]:
+
+
+
+
